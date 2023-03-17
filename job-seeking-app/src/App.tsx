d7ffc116c53206caf81f34552d7f5e98e60a38e6
firas <firas.moukhallalaty@gmail.com>
2023-03-17 22:21:57 +0200

    chifted project to typescript

--- job-seeking-app/src/App.tsx
+++ job-seeking-app/src/App.tsx
@@ -1,9 +1,11 @@
+import React from 'react';
 import './App.css';
+import NavBar from './components/nav-bar/NavBar';
 
 function App() {
   return (
     <div className="App">
-      
+      <NavBar></NavBar>
     </div>
   );
 }
---
+++ job-seeking-app/src/components/nav-bar/NavBar.js
@@ -0,0 +1,13 @@
+import './NavBar.css';
+import resources from '../../config/resources.json';
+
+export default function NavBar() {
+  return (
+    <div className="NavBar">
+      <div className="logo-title-container">
+        <img></img>
+        <p></p>
+      </div>
+    </div>
+  );
+}
